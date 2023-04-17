cb4baafe778f11f8b97de1f1a1d39998745c7d84
suganthsugi <suganthjayanthi@gmail.com>
2023-04-17 11:16:40 +0530

    fix: updating js to srp and clean code

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 var todos = []
 var completed = []
-var id=0;
+var id = 0;
 
 document.getElementById("todoCount").innerText = '0';
 document.getElementById("completedCount").innerText = '0';
 
-const addTask = (event) => {
-    event.preventDefault();
+
+const createTodo = () => {
     const newTodo = {
         id,
         todo: document.getElementById("todoInput").value
     };
-    // console.log(newTodo);
+    return newTodo;
+}
+
+const incrementId = () => {
     id++;
+}
+
+const addTask = (event) => {
+    event.preventDefault();
+    const newTodo = createTodo();
+    incrementId();
     addTodo(newTodo);
     document.getElementById('todoInput').value = ''
 }
 
 const addTodo = (rtodo) => {
-    todos=[rtodo, ...todos];
+    todos = [rtodo, ...todos];
     // console.log(todos);
-
     updateTodoCount();
 }
 
@@ -31,38 +39,46 @@ const updateTodoCount = () => {
 }
 
 const updateTodoHtml = () => {
-    var todohtml='';
-    for (let i = 0; i < todos.length; i++) {
-        const element = todos[i];
-        if(element!==undefined){
-            todohtml+=`<li>
-                            <label class="checkbox">
-                                <input type="checkbox" id="${element.id}" onclick="removeTask(${element.id}, ${1})">
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="todoText" for="${element.id}">${element.todo}</label>
-                        </li>
-                        `
-        }
-    }
+    var todohtml = buildTodoHtml();
     document.getElementById("todoList").innerHTML = todohtml;
 }
 
+
+const buildTodoHtml = () => {
+    return todos.reduce((accu, element) => accu + `<li>
+                                                        <label class="checkbox">
+                                                            <input type="checkbox" id="${element.id}" onclick="removeTask(${element.id}, ${1})">
+                                                            <span class="checkmark"></span>
+                                                        </label>
+                                                        <label class="todoText" for="${element.id}">${element.todo}</label>
+                                                    </li>
+                                                    `,
+        '');
+}
+
+
 const removeTask = (id, choice) => {
     if (choice === 1) {
-        rtodo = todos.filter((obj) => obj.id === id)[0];
-        todos.splice(todos.indexOf(rtodo), 1);
-        updateTodoCount();
-        addCompleted(rtodo);
-    } 
+        removeFromTodo(id)
+    }
     else {
-        rtodo = completed.filter((obj) => obj.id === id)[0];
-        completed.splice(completed.indexOf(rtodo), 1);
-        updateCompletedCount();
-        addTodo(rtodo);
+        removeFromCompleted(id);
     }
 }
 
+const removeFromTodo = (id) => {
+    rtodo = todos.filter((obj) => obj.id === id)[0];
+    todos.splice(todos.indexOf(rtodo), 1);
+    updateTodoCount();
+    addCompleted(rtodo);
+}
+
+const removeFromCompleted = () => {
+    rtodo = completed.filter((obj) => obj.id === id)[0];
+    completed.splice(completed.indexOf(rtodo), 1);
+    updateCompletedCount();
+    addTodo(rtodo);
+}
 
 const addCompleted = (rtodo) => {
     completed = [...completed, rtodo];
@@ -78,21 +94,18 @@ const updateCompletedCount = () => {
 
 
 const updateCompletedHtml = () => {
-    var completedHtml = '';
-    for (let i = 0; i < completed.length; i++) {
-        const element = completed[i];
-        // console.log(element);
-        if(element!==undefined){
-            completedHtml+=`<li>
-                                <label class="checkbox">
-                                    <input type="checkbox" id="${element.id}" onclick="removeTask(${element.id}, ${2})" checked>
-                                    <span class="checkmark"></span>
-                                </label>
-                                <label for="${element.id}" class="todoText"><s>${element.todo}</s></spen>
-                            </li>
-                        `
-        }
-    }
-    // console.log(completedHtml);
+    var completedHtml = buildCompletedHtml();
     document.getElementById("completedList").innerHTML = completedHtml;
+}
+
+const buildCompletedHtml = () => {
+    return completed.reduce((accu, element) => accu + `<li>
+                                                        <label class="checkbox">
+                                                            <input type="checkbox" id="${element.id}" onclick="removeTask(${element.id}, ${2})" checked>
+                                                            <span class="checkmark"></span>
+                                                        </label>
+                                                        <label for="${element.id}" class="todoText"><s>${element.todo}</s></spen>
+                                                    </li>
+                                                    `,
+        '');
 }
